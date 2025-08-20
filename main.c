cf3126461815fc2f8d9e3f050a3f88fbff49ebd1
Ailton_bezerra <ailtonbsilva96@gmail.com>
2025-08-20 18:45:00 -0300

    add main file
    Inicial version

---
+++ main.c
@@ -0,0 +1,126 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_board(char **new_board, int height, int width)
+{
+	for (int row = 0; row < height; row++)
+    {
+		for (int col = 0; col < width; col++)
+            putchar(new_board[row][col]);
+        putchar('\n');
+    }
+}
+
+void free_board(char **board, int height)
+{
+    for (int row = 0; row < height; row++)
+        free(board[row]);
+    free(board);
+}
+
+char **start_board(char **new_board, int width, int height)
+{
+    char c;
+    int index = 0;
+    int array = 0;
+    int drawing = 0;
+    while (read(STDIN_FILENO, &c, 1) > 0)
+    {
+        if (c == '\n')
+            continue;
+        if (new_board[array] == NULL)
+			break;
+		if (c == 'x')
+			drawing = !drawing;
+		else if (c == 'w' && array > 0)
+			array--;
+		else if (c == 'a' && index > 0)
+			index--;
+		else if (c == 's' && (array + 1 < height))
+			array++;
+		else if (c == 'd' && (index + 1 < width))
+			index++;
+		if (drawing && new_board[array] && index < width)
+			new_board[array][index] = '0';
+    }
+	return (new_board);
+}
+
+char **board_creation(int width, int height)
+{
+	char **new_board = calloc(height, sizeof(char *));
+	for (int row = 0; row < height; row++)
+	{
+		new_board[row] = calloc(width, sizeof(char));	
+		for (int col = 0; col < width; col++)
+			new_board[row][col] = ' ';
+	}
+	return (new_board);
+}
+
+int count_neighbors(char **board, int row, int col, int width, int height )
+{
+	int count = 0;
+	for (int dr = -1; dr <= 1; dr++)
+	{
+		for (int dc = -1; dc <= 1; dc++)
+		{
+			if (dr == 0 && dc == 0)
+				continue;
+			int nr = row + dr;
+			int nc = col + dc;
+			if (nr >= 0 && nr < height && nc < width)
+				if (board[nr][nc] == '0')
+					count++;
+		}
+	}
+	return (count);
+}
+
+char **next_gen(char **board, int width, int height)
+{
+	char **new_board = board_creation(width, height);
+	for (int row = 0; row < height; row++)
+	{
+		for (int col = 0; col < width; col++)
+		{
+			int neighbors = count_neighbors(board, row, col, width, height);
+			if (board[row][col] == '0')
+			{
+				if (neighbors == 2 || neighbors == 3)
+					new_board[row][col] = '0';
+				else
+					new_board[row][col] = ' ';
+			}
+			else
+			{
+				if (neighbors == 3)
+					new_board[row][col] = '0';
+				else
+					new_board[row][col] = ' ';
+			}
+		}
+	}
+	free_board(board, height);
+	return (new_board);
+}
+
+int main (int argc, char **argv)
+{
+	if (argc == 4)
+	{
+		int width = atoi(argv[1]);
+		int height = atoi(argv[2]);
+		int max_iterations = atoi(argv[3]);
+		char **empty_board = board_creation(width, height);
+		char **board = start_board(empty_board, width, height);
+		if (!board)
+			return (0);
+		while (max_iterations--)
+			board = next_gen(board, width, height);
+		print_board(board, height, width);
+		free_board(board, height);
+	}
+	return (0);
+}
